f2bf6a1675a43fcefb04880e32aa8426a8a7a95f
santosh timilsina <santos.timil@gmail.com>
2023-07-25 01:03:25 +0545

    order-service-project-setup-commit

---
+++ order-service/src/test/java/com/santosh/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
