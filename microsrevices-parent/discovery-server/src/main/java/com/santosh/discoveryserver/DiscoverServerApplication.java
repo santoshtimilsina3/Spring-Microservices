397532787f3196613d2a0c1eb5b5d43b81134d70
santosh timilsina <santos.timil@gmail.com>
2023-07-29 20:21:53 +0545

    register-discovery-client-to-server

--- microsrevices-parent/discovery-server/src/main/java/com/santosh/discoveryserver/DiscoverServerApplication.java
+++ microsrevices-parent/discovery-server/src/main/java/com/santosh/discoveryserver/DiscoverServerApplication.java
@@ -1,2 +1,15 @@
-package com.santosh.discoveryserver;public class DiscoverServerApplication {
+package com.santosh.discoveryserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoverServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverServerApplication.class,args);
+    }
 }
+
